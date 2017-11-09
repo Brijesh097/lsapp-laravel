edad8d592596b1e5531ef889f3852266eca724e8
Brijesh <brijesh.reddy15@siesgst.ac.in>
2017-11-09 10:31:08 +0530

    Blade Templating

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,16 +8,30 @@ class PagesController extends Controller
 {
     // Return index view
     public function index () {
-        return view('pages.index');
+        $title = 'Welcome to Laravel';
+
+        // Passing value into blade template using WITH method
+        return view('pages.index')->with('title', $title);
     }
 
     // Return about view
     public function about () {
-        return view('pages.about');
+        $title = 'About Page';
+
+        // Passing value into blade template using COMPACT function
+        return view('pages.about', compact('title'));
     }
 
     //Return services view
     public function services () {
-        return view('pages.services');
+
+        // Created an associative array containing the title & services to be provided
+        $data = array(
+            'title'    => 'Services Page',
+            'services' => ['Web Design', 'Programming', 'SEO']
+        );
+
+        // Passing value into blade template using WITH method
+        return view('pages.services')->with($data);
     }
 }
--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,14 +1,13 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{config('app.name', 'LSAPP')}}</title>
-    </head>
-    <body>
-        <h1>Services Page</h1>
-        <p>This is the services page!</p>
-    </body>
-</html>
+@section('content')
+    <h1>{{$title}}</h1>
+    <p>These are the services provided by us:</p>
+    @if(count($services) > 0)
+        <ul>
+            @foreach($services as $service)
+                <li>{{$service}}</li>
+            @endforeach
+        </ul>
+    @endif
+@endsection
