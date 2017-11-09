490d8dc5f860fcab42026f7dec907a3a8377ff9d
Brijesh <brijesh.reddy15@siesgst.ac.in>
2017-11-09 14:35:30 +0530

    Assets linked

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,27 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{config('app.name', 'LSAPP')}}</title>
+
+        <!-- Bootstrap Stylesheet -->        
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+        <!-- Internal style -->
+        <style>
+            .fix {
+                margin-top: 65px;
+            }
+            h1 {
+                font-size: 45px !important;
+            }
+        </style>
     </head>
     <body>
-        @yield('content')
+        <!-- Navbar -->
+        @include('inc.navbar')
+        
+        <!-- Content -->
+        <div class="container fix">
+            @yield('content')        
+        </div>
     </body>
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{$title}}</h1>
-    <p>This is the index page!</p>
+    <div class="jumbotron">
+      <div class="container text-center">
+        <h1>{{$title}}</h1>
+        <p>This is a simple web application created using laravel framework ...</p>
+        <br>
+        <p>
+            <a class="btn btn-primary btn-md" href="/login" role="button">Login</a>
+            &nbsp;
+            <a class="btn btn-success btn-md" href="/register" role="button">Register</a>
+        </p>
+      </div>
+    </div>
 @endsection
