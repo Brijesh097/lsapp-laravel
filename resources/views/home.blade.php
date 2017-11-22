2b4a98406b79363f6a14ccf22135fde80f792710
Brijesh <brijesh.reddy15@siesgst.ac.in>
2017-11-23 01:51:44 +0530

    Model Relationships

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">My Posts</div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -17,6 +17,32 @@
                     <a href="/posts/create" class="btn btn-primary">Create Post</a>
                     <hr>
                     <h3><b>Your Posts :</b></h3>
+                    @if(count($posts) > 0)
+                        <table class="table table-striped table-hover">
+                            <thead>
+                                <th>TITLE</th>
+                                <th></th>
+                                <th></th>
+                            </thead>
+                            <tbody>
+                                @foreach($posts as $post)
+                                    <tr>
+                                        <td>{{$post->title}}</td>
+                                        <td><a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a></td>
+                                        <td>
+                                            {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+                                                {{Form::hidden('_method', 'DELETE')}}
+                                                {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                                            {!!Form::close()!!}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    @else
+                        <hr>
+                        <p>You have no posts.</p>
+                    @endif
                 </div>
             </div>
         </div>
